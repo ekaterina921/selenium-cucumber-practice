e07346083634f4ebadd1321fd4b335bb7804ce7f
ekaterinamq <ma_ka_r0va@bk.ru>
2023-05-30 21:05:43 +0200

    Update DURATIONOFWAITING constant name

--- src/test/java/pageobject_model/ExplicitWaits.java
+++ src/test/java/pageobject_model/ExplicitWaits.java
@@ -8,25 +8,26 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class ExplicitWaits {
-    public static final int DURATIONOFWAITING = 10;
+    public static final int MEDIUM_DURATION_OF_WAITING = 10;
     public static void waitPage(WebDriver driver, String pageTitle) {
-        (new WebDriverWait(driver, Duration.ofSeconds(DURATIONOFWAITING))).
+        (new WebDriverWait(driver, Duration.ofSeconds(MEDIUM_DURATION_OF_WAITING))).
                 until(ExpectedConditions.
+
                         textToBePresentInElement(driver.findElement(By.className("title")), pageTitle));
     }
 
     public static void waitPageByElementId(WebDriver driver, String elementId) {
-        (new WebDriverWait(driver, Duration.ofSeconds(DURATIONOFWAITING))).
+        (new WebDriverWait(driver, Duration.ofSeconds(MEDIUM_DURATION_OF_WAITING))).
                 until(ExpectedConditions.presenceOfElementLocated(By.id(elementId)));
     }
 
     public static void waitRemoveButton(WebDriver driver) {
-        (new WebDriverWait(driver, Duration.ofSeconds(DURATIONOFWAITING))).
+        (new WebDriverWait(driver, Duration.ofSeconds(MEDIUM_DURATION_OF_WAITING))).
                 until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='Remove']")));
     }
 
     public static void waitAddToCartButton(WebDriver driver) {
-        (new WebDriverWait(driver, Duration.ofSeconds(DURATIONOFWAITING))).
+        (new WebDriverWait(driver, Duration.ofSeconds(MEDIUM_DURATION_OF_WAITING))).
                 until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='Add to cart']")));
     }
 }
