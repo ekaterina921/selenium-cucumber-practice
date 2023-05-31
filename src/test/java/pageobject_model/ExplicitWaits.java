c6b41ebda658209784561ec05ae94222ec4cde25
ekaterinamq <ma_ka_r0va@bk.ru>
2023-05-31 20:43:44 +0200

    Add waitButton method instead of two methods waitRemoveButton and waitAddToCartButton

--- src/test/java/pageobject_model/ExplicitWaits.java
+++ src/test/java/pageobject_model/ExplicitWaits.java
@@ -20,13 +20,8 @@ public class ExplicitWaits {
                 until(ExpectedConditions.presenceOfElementLocated(By.id(elementId)));
     }
 
-    public static void waitRemoveButton(WebDriver driver) {
+    public static void waitButton(WebDriver driver, String xPath) {
         (new WebDriverWait(driver, Duration.ofSeconds(MEDIUM_DURATION_OF_WAITING))).
-                until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='Remove']")));
-    }
-
-    public static void waitAddToCartButton(WebDriver driver) {
-        (new WebDriverWait(driver, Duration.ofSeconds(MEDIUM_DURATION_OF_WAITING))).
-                until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='Add to cart']")));
+                until(ExpectedConditions.presenceOfElementLocated(By.xpath(xPath)));
     }
 }
--- src/test/java/pageobject_model/InventoryItemsActions.java
+++ src/test/java/pageobject_model/InventoryItemsActions.java
@@ -6,12 +6,12 @@ import org.openqa.selenium.WebDriver;
 public interface InventoryItemsActions {
     default void addToCart(WebDriver driver, String name) {
         driver.findElement(By.name(name)).click();
-        ExplicitWaits.waitRemoveButton(driver);
+        ExplicitWaits.waitButton(driver,"//*[text()='Remove']");
     }
 
     default void removeFromCart(WebDriver driver, String name) {
         driver.findElement(By.name(name)).click();
-        ExplicitWaits.waitAddToCartButton(driver);
+        ExplicitWaits.waitButton(driver, "//*[text()='Add to cart']");
     }
 
 
