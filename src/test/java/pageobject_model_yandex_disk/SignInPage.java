9b6bb2b39be7da6f6866cc9ae73cf396c0531de4
ekaterinamq <ma_ka_r0va@bk.ru>
2023-06-06 22:15:37 +0200

    Remove credentials from SignInPage Update url for Sauce Labs website

--- src/test/java/pageobject_model_yandex_disk/SignInPage.java
+++ src/test/java/pageobject_model_yandex_disk/SignInPage.java
@@ -4,16 +4,15 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 
 public class SignInPage extends YandexDiskPages {
-    static final String USERNAME = "ekaterinamtest";
-    static final String PASSWORD = "KatsiarynaMTest08";
+
     static final String SIGN_IN_BUTTON_ID = "passp:sign-in";
-    public ClientDiskPage signIn(WebDriver driver) {
+    public ClientDiskPage signIn(WebDriver driver, String username, String password) {
         Actions actions = new Actions(driver);
-        actions.click(driver.findElement(By.id("passp-field-login"))).sendKeys(USERNAME).sendKeys(Keys.RETURN).build().perform();
+        actions.click(driver.findElement(By.id("passp-field-login"))).sendKeys(username).sendKeys(Keys.RETURN).build().perform();
         WebElement nextButton = driver.findElement(By.id(SIGN_IN_BUTTON_ID));
         JavascriptExecutor jsExec = (JavascriptExecutor) driver;
         jsExec.executeScript("arguments[0].click();", nextButton);
-        driver.findElement(By.id("passp-field-passwd")).sendKeys(PASSWORD);
+        driver.findElement(By.id("passp-field-passwd")).sendKeys(password);
         driver.findElement(By.id(SIGN_IN_BUTTON_ID)).click();
         return new ClientDiskPage();
     }
